db0a3f4a7b042ac61b87f18ce12c8382efaed60c
DWebsterJr <dwebs007@odu.edu>
2015-03-16 11:21:46 -0400

    milestone sql 2

--- milestone1dump.sql
+++ milestone1dump.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `stack`
 --
-CREATE DATABASE IF NOT EXISTS `stack` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `stack` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `stack`;
 
 -- --------------------------------------------------------
