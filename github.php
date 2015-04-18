6ec4379879612c50c6ff434d9bc007c7b0790215
DWebsterJr <DWebsterJr@users.noreply.github.com>
2015-04-18 18:37:43 -0400

    Update github.php

--- github.php
+++ github.php
@@ -3,7 +3,7 @@
 $clientId="725e08a43209c83e9833";
 $clientSecrect ="70da2c35ff3a63ee2c04bc10a90083003ed9a7ea";
 
-	if($_GET && isset($_GET['code'])){
+	if(isset($_GET['code'])){
 
 		$ch = curl_init();
 		curl_setopt($ch, CURLPT_URL, "https://github.com/login/oauth/access_token");
